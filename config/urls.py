64638738e7715c68e4205a705f9ab60b4cc2d6e1
woniu <2463396298@qq.com>
2026-06-25 09:28:26 +0800

    urls: 惰性 ExtensionRouter 从 entry points 发现扩展路由

--- config/urls.py
+++ config/urls.py
@@ -6,30 +6,62 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import JsonResponse
 
 from bias_core.api_runtime import build_api_application
-from bias_core.extensions.bootstrap import get_extension_host
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
 
-# Build the API during startup
-api = build_api_application(extension_host=get_extension_host())
+class LazyMount:
+    def __init__(self, prefix, router, tags=None):
+        self.prefix = prefix
+        self.router = router
+        self.tags = tags or []
+
+
+class ExtensionRouter:
+    def __init__(self):
+        self._mounts = None
+
+    def make(self, key):
+        if key == "routes":
+            return self
+        raise AttributeError(key)
+
+    def get_mounts(self):
+        if self._mounts is None:
+            self._mounts = self._discover()
+        return self._mounts
+
+    def _discover(self):
+        import importlib.metadata
+        mounts = []
+        for ep in importlib.metadata.entry_points(group="bias.extensions"):
+            name = ep.name
+            for sub in [("api", "/" + name), ("admin_api", "/admin")]:
+                try:
+                    mod = importlib.import_module("bias_ext_%s.backend.%s" % (name, sub[0]))
+                    router = getattr(mod, "router", None)
+                    if router is not None:
+                        mounts.append(LazyMount(sub[1], router, [name.title()]))
+                except Exception:
+                    pass
+        return mounts
+
+
+api = build_api_application(extension_host=ExtensionRouter())
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 def rebuild_api_urlpatterns():
-    """Rebuild URL patterns after extension runtime changes."""
-    global urlpatterns
-    api = build_api_application(extension_host=get_extension_host())
+    global api, urlpatterns
+    api = build_api_application(extension_host=ExtensionRouter())
     urlpatterns = [
         path("admin/", admin.site.urls),
         path("api/", api.urls),
